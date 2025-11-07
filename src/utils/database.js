580f44b108b1694498d2ce7bcd3f1aa03b4d6277
Nadila Pilar Kencana <60565659+nadilakencana@users.noreply.github.com>
2025-11-07 11:25:53 +0700

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_DATABASE || 'nutech_db',
+    database: process.env.DB_DATABASE || 'railway',
     port: process.env.DB_PORT || 5432,
     allowExitOnIdle: true
 });
